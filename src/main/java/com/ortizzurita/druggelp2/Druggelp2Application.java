8fea20221353b8fdd1f608af9db8979df02a7f19
KassieZuritaG <57915876+KassieZuritaG@users.noreply.github.com>
2020-08-02 16:22:07 -0500

    Priner commit Plantilla
    Proyecto con plantilla

---
+++ src/main/java/com/ortizzurita/druggelp2/Druggelp2Application.java
@@ -0,0 +1,13 @@
+package com.ortizzurita.druggelp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Druggelp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Druggelp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ortizzurita/druggelp2/Druggelp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortizzurita.druggelp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Druggelp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
